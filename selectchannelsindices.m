dae2e3d63bf298ddf7a1ad68be5006a307d0608f
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-06-20 14:53:05 +0200

    adds fiunction to select indices rather than numberonamplifier

---
+++ selectchannelsindices.m
@@ -0,0 +1,16 @@
+function [ selectedChannelsIndices ] = selectchannelsindices(  Header, signalTypes )
+%SELECTCHANNELS returns a vector of channel indices that are of a given recorging type
+%   signalTypes: cell with strings of signal types, e.g. {'SEEG', 'ECoG-Grid', 'ECoG-Strip'};  
+    
+%VALIDATE signal types
+    selectedChannelsIndices = [];
+    for channelIndex = 1:size(Header.channels, 2)
+        if isfield(Header.channels(channelIndex), 'signalType')  % if the field exists - if it doesn't, this should really return something
+            if ismember(Header.channels(channelIndex).signalType, signalTypes)
+                selectedChannelsIndices = [selectedChannelsIndices, i];
+            end
+        else
+            fprintf('Channel %d struct does not have singalType field in header', channelIndex);
+        end
+    end
+end

115715f447f3b9e63906ab6631e59450648949c7
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-06-20 12:28:55 +0200

    adds function to check matlab version to deal with input parsers

---
+++ matlabversion.m
@@ -0,0 +1,8 @@
+function [ strct ] = matlabversion()
+%MATLABVERSION Summary of this function goes here
+%   Detailed explanation goes here
+    split = strsplit(version,' ');
+    strct = struct();
+    [match, nomatch] = regexp('R2010b','\d+','match','split');
+    strct.year = str2double(match);
+end

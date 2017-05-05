849928d58dd840ecb33655ab01291237990eb97f
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-05-05 14:36:55 +0200

    removes NaN electrodes from the list

--- Grouping/perelectrodegrouping.m
+++ Grouping/perelectrodegrouping.m
@@ -3,13 +3,14 @@ function [ channelGroups ] = perelectrodegrouping( Header, selectedChannelsIndic
 %   In case all channels from one electrode are discarded, electrode is returned as empty - ISSUE, fix it
     electrodesFullNames = extractfield(Header.channels, 'name');
     electrodesNames = cellfun(@(x) extractfromstring(x, 'string'), electrodesFullNames, 'UniformOutput', false);
+    electrodesNames = selectcells(electrodesNames,  @(x) ~isnan(x));
    	uniqueElectrodesNames = unique(electrodesNames);
     channelGroups = cell(1,size(uniqueElectrodesNames,2));
     
-    for electrodeIndex = 1:size(uniqueElectrodesNames,2)
+    for electrodeIndex = 1:size(uniqueElectrodesNames, 2)
         electrodeName = uniqueElectrodesNames(electrodeIndex);
         channelsThatBelong = cellfun(@(x) strcmp(electrodeName, x) ,electrodesNames, 'UniformOutput', true);
         channelsIndices = find(channelsThatBelong);
-        channelGroups{electrodeIndex} = intersect(channelsIndices,selectedChannelsIndices);
+        channelGroups{electrodeIndex} = intersect(channelsIndices, selectedChannelsIndices);
     end
 end
